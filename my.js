b7f0289f8fb6a613728c29c13273e2450410ba46
Nguyen-Tu-Anh <nguyentuanh.10t2@gmail.com>
2021-10-08 08:50:10 +0700

    first commit

---
+++ my.js
@@ -0,0 +1,8 @@
+function check_dap_an() {
+ let anSwer = document.getElementById('answer').value;
+ if (anSwer == 'ECMAScript') {
+     alert('Right')
+ } else {
+     alert("You don't know? ECMAScript!")
+ }
+}
